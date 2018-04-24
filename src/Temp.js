f9673eb7afa73c6c6a3809bd4fbd7829f88df4bc
valiirshad <irshad@xelpmoc.in>
2018-04-24 17:00:32 +0530

    new icons

--- src/Temp.js
+++ src/Temp.js
@@ -1,27 +1,53 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react';
 
 import {
     View,
     WebView,
-    StyleSheet
-} from 'react-native'
+    StyleSheet,
+    StatusBar,
+    Dimensions,
+    Platform,
+} from 'react-native';
+const height = Dimensions.get('window').height;
 const Temp = () => {
-    return (
-        <View style={styles.container}>
-            <WebView
-                source={{ uri: 'https://alpha.woovly.com' }}
-            />
-        </View>
-    )
-}
+    console.log('Height', height);
+    return ( <
+        View style = {
+            styles.container
+        } >
+        <
+        StatusBar translucent = {
+            true
+        }
+        barStyle = {
+            'light-content'
+        }
+        /> <
+        WebView source = {
+            {
+                uri: 'https://alpha.woovly.com'
+            }
+        }
+        style = {
+            {
+                marginTop: Platform.OS === 'ios' ? 20 : 0
+            }
+        }
+        /> < /
+        View >
+    );
+};
 Temp.navigationOptions = {
-    title: "",
+    title: '',
 };
 export default Temp;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        height: height,
         flexDirection: 'column',
-    }
-})
+        backgroundColor: '#505F69',
+    },
+});
